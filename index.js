2eacd5f05fdd0c657b94954fd2d30f12c8ef8258
Remberto <rembertobeltran@icloud.com>
2022-05-25 12:10:10 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employee) {
+    let emp = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+    return emp;
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map((employee) => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(stamp) {
+    let [date, hour] = stamp.split(' ');
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date,
+    });
+    return this;
+}
+
+function createTimeOutEvent(stamp) {
+    let [date, hour] = stamp.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date,
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(stamp) {
+    let clockIn = this.timeInEvents.find((timeIn) => timeIn.date == stamp);
+    let clockOut = this.timeOutEvents.find((timeOut) => timeOut.date == stamp);
+    return (clockOut.hour - clockIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(stamp) {
+    return hoursWorkedOnDate.call(this, stamp) * this.payPerHour;
+}
+
+
+function fineEmployeeByFirstName(employees, employeeName) {
+    return employees.find((employee) => employee.firstName == employeeName)
+}
+
+ function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => {
+       return total + allWagesFor.call(employee);
+    }, 0);
+ }
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +77,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
